969e2e0d162863c3749a33a3337f9270be79b75c
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-12-31 12:26:53 +0530

    create product api created

--- server/src/product/product.controller.ts
+++ server/src/product/product.controller.ts
@@ -1,12 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Body, Controller, Post } from '@nestjs/common';
 
+import { ProductService } from './product.service';
+
 @Controller('product')
 export class ProductController {
+  constructor(private readonly productService: ProductService) {}
   @Post()
   createProduct(
     @Body('name') name: string,
-    @Body('price') price: string,
+    @Body('price') price: number,
     @Body('description') description?: string,
-  ) {}
+  ) {
+    return this.productService.create(name, price, description);
+  }
 }
--- server/src/product/product.module.ts
+++ server/src/product/product.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import { ProductSchema } from './product.shcema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]),
+  ],
   controllers: [ProductController],
   providers: [ProductService],
 })
